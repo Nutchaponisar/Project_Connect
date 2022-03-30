d8fddc0e4023ebdb0d9f8d91d4c20a1d7cf3c71d
Nutchaponisar <nutchapon.isar@bumail.net>
2022-03-30 17:07:23 +0700

    new

---
+++ Assets/Project_Game/Scripts/NotUseNowOrDelete/ObjectClick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ObjectClick : NetworkBehaviour
+{
+    /*[SerializeField] private Transform[] players = new Transform[2];
+    [SerializeField] private LineController line;
+    private TargetClick target;
+    private void Start()
+    {
+        
+        line.SetUpLine(players);
+        if (isLocalPlayer)
+        {
+            players[0] = this.gameObject.transform;
+        }
+    }
+    private void Update()
+    {
+        line.SetUpLine(players);
+    }
+    private void Click()
+    {
+        line.enabled = true;
+        if(target.isClick = true)
+        {
+            players[1] = target.objecthit;
+        }
+    }*/
+}
